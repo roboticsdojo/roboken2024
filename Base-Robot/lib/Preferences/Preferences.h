fa1553696cab61b017dd27d3bdd7df1fa00407bb
WahomeKennedy <kennedywahome.24@gmail.com>
2024-08-06 06:52:17 +0300

    porting previous code

---
+++ Base-Robot/lib/Preferences/Preferences.h
@@ -0,0 +1,22 @@
+#ifndef PREFERENCES_H
+#define PREFERENCES_H
+
+#define MOTOR_SPEED 180
+
+#define ENABLE_LEFT_MOTOR 45 //3 //6
+#define ENABLE_RIGHT_MOTOR 46 //5
+#define LEFT_MOTOR_PIN1 8
+#define LEFT_MOTOR_PIN2 9
+#define RIGHT_MOTOR_PIN1 10
+#define RIGHT_MOTOR_PIN2 4
+
+#define CORRECTION 100
+
+#define RIGHT_ENCODER_A_PIN 18 //18 //2 (2 and 20 for left)
+#define RIGHT_ENCODER_B_PIN 19 //19 //3 (18 and 19 for right)
+
+// ENCODERS
+#define LEFT_ENCODER_A_PIN 3//20 //18 //2 (2 and 20 for left)
+#define LEFT_ENCODER_B_PIN 2 //19 //3 (18 and 19 for right)
+
+#endif
